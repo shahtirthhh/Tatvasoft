b8a859da5bcdaf84186c5ef06a7eb7ed0b67528a
Tirth shah <shahtirth0212@gmail.com>
2023-03-05 12:17:10 +0530

    Day 1 task done

---
+++ tasks/day1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hello from './components/Hello';
+function App() {
+  return (
+    <div>
+      <h1>This is App</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ tasks/day1/src/components/Hello.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Hello() {
+    return (<h1>This is Hello Component</h1>)
+}
+export default Hello;
